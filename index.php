989e5d72c21684ee7261c53e879d9b69751daea5
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-11 14:39:32 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>patie 9 exercice 1</title>
+</head>
+<body>
+    <?php $date = date('d/m/Y') ?>
+    <p><?= 'nous somme le ' . $date ?></p>
+</body>
+</html>
